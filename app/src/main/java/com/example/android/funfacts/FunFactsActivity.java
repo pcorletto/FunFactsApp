d2df66a32f2c52eddd6b2536e96bd636ecdca58b
Peter Corletto <peter.corletto@gmail.com>
2015-11-02 18:21:12 -0500

    6th commit

--- app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.funfacts;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class FunFactsActivity extends ActionBarActivity {
@@ -12,6 +14,11 @@ public class FunFactsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Declare our view variables and assign them the Views from the layout file.
+        TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
     }
 
 
