5d7eee1c18160e8c4100eb6f5ce3c414f512b646
Peter Corletto <peter.corletto@gmail.com>
2015-11-06 18:09:24 -0500

    After adding an array of Strings and making my own prediction on the code

--- app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
@@ -15,6 +15,20 @@ public class FunFactsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final String[] facts = {
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver.",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the Sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water.",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the earth when the Great Pyramid was being built."
+        };
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
 
@@ -28,7 +42,7 @@ public class FunFactsActivity extends ActionBarActivity {
             String fact="";
             // Randomly select a fact
             Random randomGenerator = new Random();   // Construct a new Random number generator
-                int randomNumber = randomGenerator.nextInt(3);
+                int randomNumber = randomGenerator.nextInt(facts.length);
 
                 // We'll make our app only have 3 random facts
 
@@ -38,7 +52,7 @@ public class FunFactsActivity extends ActionBarActivity {
                 *1 = Ostriches can run faster than horses
                 *2 = Olympic gold medals are actually made mostly of silver
 
-                */
+
 
                 // If randomNumber = 0
                 if(randomNumber == 0){
@@ -61,6 +75,9 @@ public class FunFactsActivity extends ActionBarActivity {
                 else {
                     fact = "Sorry, there was an error";
                 }
+                */
+
+                fact = facts[randomNumber];
 
             // Update the label with our dynamic fact
             factLabel.setText(fact);
