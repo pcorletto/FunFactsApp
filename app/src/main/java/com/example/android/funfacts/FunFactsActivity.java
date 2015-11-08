4f76ca8a04c53ebab3bc041bc4ebc2ac1d3f186d
Peter Corletto <peter.corletto@gmail.com>
2015-11-08 18:48:36 -0500

    After creating and instantiating a FactBook object and refactoring the code

--- app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
@@ -6,10 +6,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Random;
-
 
 public class FunFactsActivity extends ActionBarActivity {
+
+    private FactBook mFactBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,36 +19,17 @@ public class FunFactsActivity extends ActionBarActivity {
         // Declare our view variables and assign them the Views from the layout file.
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                final String[] facts = {
-                    "Ants stretch when they wake up in the morning.",
-                    "Ostriches can run faster than horses.",
-                    "Olympic gold medals are actually made mostly of silver.",
-                    "You are born with 300 bones; by the time you are an adult you will have 206.",
-                    "It takes about 8 minutes for light from the Sun to reach Earth.",
-                    "Some bamboo plants can grow almost a meter in just one day.",
-                    "The state of Florida is bigger than England.",
-                    "Some penguins can leap 2-3 meters out of the water.",
-                    "On average, it takes 66 days to form a new habit.",
-                    "Mammoths still walked the earth when the Great Pyramid was being built.",
-                    "Treehouse is actually not in a tree."
-            };
-
-            // The button was clicked, so update the fact label with a new label
-            String fact="";
-            // Randomly select a fact
-            Random randomGenerator = new Random();   // Construct a new Random number generator
-            int randomNumber = randomGenerator.nextInt(facts.length);
-
-            fact = facts[randomNumber];
-
+            String fact = mFactBook.getFact();
 
             // Update the label with our dynamic fact
             factLabel.setText(fact);
-        }
+
+            }
         };
         showFactButton.setOnClickListener(listener);
 
