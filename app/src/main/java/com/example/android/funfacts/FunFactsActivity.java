2fc867185c800f76ed368a7bebbb1ff40cf7de48
Peter Corletto <peter.corletto@gmail.com>
2015-11-09 19:18:12 -0500

    After dynamically changing background color: my own attempt before listening to instructor. I could have done some refactoring here, but it works!

--- app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.funfacts;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -19,16 +21,22 @@ public class FunFactsActivity extends ActionBarActivity {
         // Declare our view variables and assign them the Views from the layout file.
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout screen = (RelativeLayout) findViewById(R.id.screen);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             String fact = mFactBook.getFact();
+            String color = mFactBook.getColor();
 
             // Update the label with our dynamic fact
             factLabel.setText(fact);
 
+            //Dynamically change the background color
+            int color2 = Color.parseColor(color);
+
+            screen.setBackgroundColor(color2);
             }
         };
         showFactButton.setOnClickListener(listener);
