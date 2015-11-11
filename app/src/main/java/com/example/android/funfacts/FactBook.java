1be39ad9f2a2633ca270829ef11b6c82d9e897a9
Peter Corletto <peter.corletto@gmail.com>
2015-11-10 22:55:59 -0500

    After adding app icon

--- app/src/main/java/com/example/android/funfacts/FactBook.java
+++ app/src/main/java/com/example/android/funfacts/FactBook.java
@@ -19,7 +19,7 @@ public class FactBook {
             "Some bamboo plants can grow almost a meter in just one day.",
             "The state of Florida is bigger than England.",
             "Some penguins can leap 2-3 meters out of the water.",
-            "On average, it takes 66 days to form a new habit.",
+            "On average, it takes 66 days to form a new habit..",
             "Mammoths still walked the earth when the Great Pyramid was being built.",
             "Treehouse is actually not in a tree."
     };
